e8b9333e1d670eee72d62e2b289f0a3d8f743d67
nilgil <nilgil183@gmail.com>
2025-06-09 22:01:17 +0900

    initialize project

---
+++ README.md
@@ -0,0 +1,11 @@
+## About
+판매자와 소비자 간의 상품 거래를 중개하는 일반적인 커머스 플랫폼입니다.
+
+## Stack
+
+- Kotlin 1.9
+- JDK 21
+- Spring Boot 3.5
+- Gradle 8.14
+- MySQL 8.0
+- JPA (Hibernate 6.6)
---
+++ src/main/kotlin/com/nilgil/commerce/GenericCommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.nilgil.commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GenericCommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GenericCommerceApplication>(*args)
+}
